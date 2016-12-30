cfc66bf5cb0fcf69eeecc267934fd7e4d08032e8
Daniel Reid <danielselareid40@gmail.com>
2016-12-30 14:48:27 +0700

    yeas

--- role.miner.js
+++ role.miner.js
@@ -5,53 +5,47 @@ const roleEmergencyHarvester = require ('role.emergencyHarvester');
 module.exports = {
     run: function (room, creep) {
         creep.say('miner;');
-        var numberOfDistributors = _.sum(Game.creeps, (c) => c.memory.role == 'distributor' && c.memory.room == room.name);
-        if (numberOfDistributors <= 0) {
-            roleEmergencyHarvester.run(room, creep);
+        //changes state
+        if (creep.memory.working == true && _.sum(creep.carry) == 0) {
+            creep.memory.working = false;
         }
-        else {
-            //changes state
-            if (creep.memory.working == true && _.sum(creep.carry) == 0) {
-                creep.memory.working = false;
-            }
-            else if (creep.memory.working == false && _.sum(creep.carry) >= creep.carryCapacity) {
-                creep.memory.working = true;
-            }
+        else if (creep.memory.working == false && _.sum(creep.carry) >= creep.carryCapacity) {
+            creep.memory.working = true;
+        }
 
-            // if working if true do stuff or else mine
-            if (creep.memory.working == true) {
+        // if working if true do stuff or else mine
+        if (creep.memory.working == true) {
 
-                //if container found put transfer energy to container if container full drop energy
+            //if container found put transfer energy to container if container full drop energy
 
-                var container = creep.pos.findInRange(FIND_STRUCTURES, 1, {
-                    filter: (s) => s.structureType == STRUCTURE_CONTAINER
-                    && _.sum(s.store) < s.storeCapacity
-                })[0];
+            var container = creep.pos.findInRange(FIND_STRUCTURES, 1, {
+                filter: (s) => s.structureType == STRUCTURE_CONTAINER
+                && _.sum(s.store) < s.storeCapacity
+            })[0];
 
-                if (container) {
-                    creep.creepSpeech(room, 'droppingEnergyContainer');
-                    for (let resourceType in creep.carry) {
-                        creep.transfer(container, resourceType);
-                    }
-                }
-                else {
-                    creep.creepSpeech(room, 'droppingEnergy');
-                    for (let resourceType in creep.carry) {
-                        creep.drop(resourceType);
-                    }
+            if (container) {
+                creep.creepSpeech(room, 'droppingEnergyContainer');
+                for (let resourceType in creep.carry) {
+                    creep.transfer(container, resourceType);
                 }
             }
             else {
-
-                var extractor = room.find(FIND_MY_STRUCTURES, {filter: (s) => s.structureType == STRUCTURE_EXTRACTOR})[0];
-                if (extractor) {
-                    var mineral = room.find(FIND_MINERALS)[0];
-                    if (creep.harvest(mineral) != OK) {
-                        creep.moveTo(mineral);
-                    }
+                creep.creepSpeech(room, 'droppingEnergy');
+                for (let resourceType in creep.carry) {
+                    creep.drop(resourceType);
                 }
-
             }
+        }
+        else {
+
+            var extractor = room.find(FIND_MY_STRUCTURES, {filter: (s) => s.structureType == STRUCTURE_EXTRACTOR})[0];
+            if (extractor) {
+                var mineral = room.find(FIND_MINERALS)[0];
+                if (creep.harvest(mineral) != OK) {
+                    creep.moveTo(mineral);
+                }
+            }
+
         }
     }
 };
