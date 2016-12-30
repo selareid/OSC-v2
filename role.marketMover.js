5da11c5c45908f4ee2ea20a4a557f763a12ff97c
Daniel Reid <danielselareid40@gmail.com>
2016-12-31 06:32:56 +0700

    aw yeah

--- role.marketMover.js
+++ role.marketMover.js
@@ -8,62 +8,41 @@ const roleCarrier = require ('role.carrier');
 
 module.exports = {
     run: function (room, creep) {
-        var labQueueFirst = Memory.rooms[room].labQueue[0];
-        if (labQueueFirst && labQueueFirst.split(',').length == 2) {
-            this.labRun(room, creep, Memory.rooms[room].labQueue)
+
+        var terminal = room.terminal;
+        var orders = Memory.rooms[room].market;
+
+        if (Game.market.getOrderById(orders[0])) {
+            this.orderRun(room, creep, terminal, orders);
         }
         else {
-            this.normalRun(room, creep);
+            this.fillRun(room, creep, terminal);
         }
     },
 
-    normalRun: function (room, creep) {
+    orderRun: function (room, creep, terminal, orders) {
 
-        var terminal = room.terminal;
-        var terminalStore = _.sum(terminal.store);
-
-        var orders = Memory.rooms[room].market;
         var order = Game.market.getOrderById(orders[0]);
 
-        if (order || terminalStore == 0) {
-            if (creep.memory.working == true && creep.carry.energy == 0) {
+            if (creep.memory.working == true && _.sum(creep.carry) == 0) {
                 creep.memory.working = false;
             }
             else if (creep.memory.working == false && _.sum(creep.carry) == creep.carryCapacity) {
                 creep.memory.working = true;
             }
-        }
-        else {
-            if (creep.memory.working == false && creep.carry.energy == 0) {
-                creep.memory.working = true;
-            }
-            else if (creep.memory.working == true && _.sum(creep.carry) == creep.carryCapacity) {
-                creep.memory.working = false;
-            }
-        }
 
         if (creep.memory.working == true) {
             if (terminal) {
-                if (order || terminalStore == 0) {
-                    if (_.sum(terminal.store) < terminal.storeCapacity) {
-                        this.putStuffIntoTerminal(room, creep, terminal);
-                    }
-                    else roleCarrier.run(room, creep);
+                if (_.sum(terminal.store) < terminal.storeCapacity) {
+                    this.putStuffIntoTerminal(room, creep, terminal);
                 }
-                else {
-                    if (room.storage.store[RESOURCE_ENERGY] > 600000) {
-                        this.putStuffIntoTerminal(room, creep, terminal);
-                    }
-                    else this.pullStuffFromTerminal(room, creep, terminal);
-                }
+                else roleCarrier.run(room, creep);
             }
-            else roleCarrier.run(room, creep);
         }
         else {
             var storage = room.storage;
             if (storage) {
                 if (orders != undefined) {
-                    if (order || terminalStore == 0) {
                         if (order) {
                             var costinEnergy = Game.market.calcTransactionCost(order.amount, room.name, order.roomName);
                             if ((RESOURCE_ENERGY in terminal.store) && terminal.store[RESOURCE_ENERGY] >= costinEnergy) {
@@ -79,20 +58,6 @@ module.exports = {
                             }
                         }
                         else roleCarrier.run(room, creep);
-                    }
-                    else {
-                        if (room.storage.store[RESOURCE_ENERGY] > 600000) {
-                            this.collectEnergy(room, creep, storage);
-                        }
-                        else {
-                            for (let resourceType in creep.carry) {
-                                if (creep.transfer(storage, resourceType) == ERR_NOT_IN_RANGE) {
-                                    creep.moveTo(storage);
-                                    break;
-                                }
-                            }
-                        }
-                    }
                 }
                 else roleCarrier.run(room, creep);
             }
@@ -100,100 +65,43 @@ module.exports = {
         }
     },
 
-    labRun: function (room, creep, labQueue) {
-        var storage = room.storage;
-        var nextReaction = labQueue[0].split(',');
+    fillRun: function (room, creep) {
 
-        if (storage.store[nextReaction[0]] && storage.store[nextReaction[1]]) { //storage has needed resources
+        var terminal = room.terminal;
 
-            if (creep.memory.working == true && creep.carry.energy == 0) {
-                creep.memory.working = false;
+        if (creep.memory.working == false && _.sum(creep.carry) == 0) {
+            creep.memory.working = true;
+        }
+        else if (creep.memory.working == true && _.sum(creep.carry) == creep.carryCapacity) {
+            creep.memory.working = false;
+        }
+
+        if (creep.memory.working == true) {
+            var resourceToFill;
+
+            do {
+                resourceToFill = this.getResourceToFill(terminal).split(',');
             }
-            else if (creep.memory.working == false && _.sum(creep.carry) == creep.carryCapacity) {
-                creep.memory.working = true;
+            while (resourceToFill.length < 2);
+
+            var result = creep.withdraw(room.storage, resourceToFill[0], resourceToFill[1]);
+            switch (result) {
+                case 0:
+                    creep.memory.working = false;
+                    break;
+                case -9:
+                    creep.moveTo(rooms.storage);
+                    break;
             }
-
-            if (creep.memory.working == true) {
-                var labs = room.find(FIND_MY_STRUCTURES, {filter: (s) => s.structureType == STRUCTURE_LAB && Memory.rooms[room].labs[s.id].type == 0});
-
-                if (labs.length == 2) {
-
-                    if (creep.memory.lab == 1) {
-                        creep.memory.lab = 0;
-
-                        if (labs[0].mineralAmount > 0 && creep.carry[labs[0].mineralType] != undefined) {
-                            if (creep.transfer(labs[0], labs[0].mineralType) == ERR_NOT_IN_RANGE) {
-                                creep.moveTo(labs[0]);
-                            }
-                        }
-                        else if (labs[1].mineralAmount > 0 && creep.carry[labs[1].mineralType] != undefined) {
-                            if (creep.transfer(labs[1], labs[1].mineralType) == ERR_NOT_IN_RANGE) {
-                                creep.moveTo(labs[1]);
-                            }
-                        }
-                        else { //none of the labs have minerals or not the minerals we need
-                            if (labs[0].mineralType == undefined) {
-                                if (creep.transfer(labs[0], nextReaction[0]) == ERR_NOT_IN_RANGE) {
-                                    creep.moveTo(labs[0]);
-                                }
-                            }
-                            if (labs[1].mineralType == undefined) {
-                                if (creep.transfer(labs[1], nextReaction[1]) == ERR_NOT_IN_RANGE) {
-                                    creep.moveTo(labs[1]);
-                                }
-                            }
-                        }
-                    }
-                    else {
-                        creep.memory.lab = 1;
-
-                        if (labs[1].mineralAmount > 0 && creep.carry[labs[1].mineralType] != undefined) {
-                            if (creep.transfer(labs[1], labs[1].mineralType) == ERR_NOT_IN_RANGE) {
-                                creep.moveTo(labs[1]);
-                            }
-                        }
-                        else if (labs[0].mineralAmount > 0 && creep.carry[labs[0].mineralType] != undefined) {
-                            if (creep.transfer(labs[0], labs[0].mineralType) == ERR_NOT_IN_RANGE) {
-                                creep.moveTo(labs[0]);
-                            }
-                        }
-                        else { //none of the labs have minerals or not the minerals we need
-                            if (labs[1].mineralType == undefined) {
-                                if (creep.transfer(labs[1], nextReaction[1]) == ERR_NOT_IN_RANGE) {
-                                    creep.moveTo(labs[1]);
-                                }
-                            }
-                            if (labs[0].mineralType == undefined) {
-                                if (creep.transfer(labs[0], nextReaction[0]) == ERR_NOT_IN_RANGE) {
-                                    creep.moveTo(labs[0]);
-                                }
-                            }
-                        }
-                    }
-
-                }
-
-            }
-            else {
-                switch (Game.time % 2) {
-                    case 1:
-                        if (creep.withdraw(storage, nextReaction[0], creep.carryCapacity / 2) == ERR_NOT_IN_RANGE) {
-                            creep.moveTo(storage);
-                        }
-                        break;
-                    default:
-                        if (creep.withdraw(storage, nextReaction[1], creep.carryCapacity / 2) == ERR_NOT_IN_RANGE) {
-                            creep.moveTo(storage);
-                        }
-                        break;
-                }
-            }
-
         }
         else {
-            Memory.rooms[room].labQueue.splice(0, 1);
+            for (let resourceType in creep.carry) {
+                if (creep.transfer(terminal, resourceType) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(terminal, {reusePath: 10});
+                    break;
+                }
+            }
         }
-
     },
 
     collectEnergy: function (room, creep, storage) {
@@ -219,6 +127,15 @@ module.exports = {
                 creep.moveTo(terminal);
                 break;
             }
+        }
+    },
+    
+    getResourceToFill: function (terminal) {
+        if (terminal.store[RESOURCE_ENERGY] < 50000) {
+            return RESOURCE_ENERGY + ',' + (50000-terminal.store[RESOURCE_ENERGY])
+        }
+        else {
+            return ['',0];
         }
     }
 };
