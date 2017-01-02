cb8522e47d4066574b7762270b534e3906550fdc
Daniel Reid <danielselareid40@gmail.com>
2017-01-02 18:45:03 +0700

    because um this code worked

--- role.marketMover.js
+++ role.marketMover.js
@@ -8,41 +8,56 @@ const roleCarrier = require ('role.carrier');
 
 module.exports = {
     run: function (room, creep) {
-
-        var terminal = room.terminal;
-        var orders = Memory.rooms[room].market;
-
-        if (Game.market.getOrderById(orders[0])) {
-            this.orderRun(room, creep, terminal, orders);
-        }
-        else {
-            this.fillRun(room, creep, terminal);
-        }
+            this.normalRun(room, creep);
     },
 
-    orderRun: function (room, creep, terminal, orders) {
+    normalRun: function (room, creep) {
 
+        var terminal = room.terminal;
+        var terminalStore = _.sum(terminal.store);
+
+        var orders = Memory.rooms[room].market;
         var order = Game.market.getOrderById(orders[0]);
 
-            if (creep.memory.working == true && _.sum(creep.carry) == 0) {
+        if (order || terminalStore == 0) {
+            if (creep.memory.working == true && creep.carry.energy == 0) {
                 creep.memory.working = false;
             }
             else if (creep.memory.working == false && _.sum(creep.carry) == creep.carryCapacity) {
                 creep.memory.working = true;
             }
+        }
+        else {
+            if (creep.memory.working == false && creep.carry.energy == 0) {
+                creep.memory.working = true;
+            }
+            else if (creep.memory.working == true && _.sum(creep.carry) == creep.carryCapacity) {
+                creep.memory.working = false;
+            }
+        }
 
         if (creep.memory.working == true) {
             if (terminal) {
-                if (_.sum(terminal.store) < terminal.storeCapacity) {
-                    this.putStuffIntoTerminal(room, creep, terminal);
+                if (order || terminalStore == 0) {
+                    if (_.sum(terminal.store) < terminal.storeCapacity) {
+                        this.putStuffIntoTerminal(room, creep, terminal);
+                    }
+                    else roleCarrier.run(room, creep);
                 }
-                else roleCarrier.run(room, creep);
+                else {
+                    if (room.storage.store[RESOURCE_ENERGY] > 600000) {
+                        this.putStuffIntoTerminal(room, creep, terminal);
+                    }
+                    else this.pullStuffFromTerminal(room, creep, terminal);
+                }
             }
+            else roleCarrier.run(room, creep);
         }
         else {
             var storage = room.storage;
             if (storage) {
                 if (orders != undefined) {
+                    if (order || terminalStore == 0) {
                         if (order) {
                             var costinEnergy = Game.market.calcTransactionCost(order.amount, room.name, order.roomName);
                             if ((RESOURCE_ENERGY in terminal.store) && terminal.store[RESOURCE_ENERGY] >= costinEnergy) {
@@ -58,49 +73,24 @@ module.exports = {
                             }
                         }
                         else roleCarrier.run(room, creep);
+                    }
+                    else {
+                        if (room.storage.store[RESOURCE_ENERGY] > 600000) {
+                            this.collectEnergy(room, creep, storage);
+                        }
+                        else {
+                            for (let resourceType in creep.carry) {
+                                if (creep.transfer(storage, resourceType) == ERR_NOT_IN_RANGE) {
+                                    creep.moveTo(storage);
+                                    break;
+                                }
+                            }
+                        }
+                    }
                 }
                 else roleCarrier.run(room, creep);
             }
             else roleCarrier.run(room, creep);
-        }
-    },
-
-    fillRun: function (room, creep) {
-
-        var terminal = room.terminal;
-
-        if (creep.memory.working == false && _.sum(creep.carry) == 0) {
-            creep.memory.working = true;
-        }
-        else if (creep.memory.working == true && _.sum(creep.carry) == creep.carryCapacity) {
-            creep.memory.working = false;
-        }
-
-        if (creep.memory.working == true) {
-            var resourceToFill;
-
-            do {
-                resourceToFill = this.getResourceToFill(terminal).split(',');
-            }
-            while (resourceToFill.length < 2);
-
-            var result = creep.withdraw(room.storage, resourceToFill[0], resourceToFill[1]);
-            switch (result) {
-                case 0:
-                    creep.memory.working = false;
-                    break;
-                case -9:
-                    creep.moveTo(room.storage);
-                    break;
-            }
-        }
-        else {
-            for (let resourceType in creep.carry) {
-                if (creep.transfer(terminal, resourceType) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(terminal, {reusePath: 10});
-                    break;
-                }
-            }
         }
     },
 
@@ -127,15 +117,6 @@ module.exports = {
                 creep.moveTo(terminal);
                 break;
             }
-        }
-    },
-    
-    getResourceToFill: function (terminal) {
-        if (terminal.store[RESOURCE_ENERGY] < 50000) {
-            return RESOURCE_ENERGY + ',' + (50000-terminal.store[RESOURCE_ENERGY])
-        }
-        else {
-            return ['',0];
         }
     }
 };
