92bd24b150feff2a061c8677fd624875f5189f97
Daniel Reid <danielselareid40@gmail.com>
2017-02-25 10:34:44 +0700

    patched bug

--- role.wallBreaker.js
+++ role.wallBreaker.js
@@ -26,7 +26,7 @@ module.exports = {
 
                                     switch (attackStructureResult) {
                                         case -9: // returns ERR_NOT_IN_RANGE
-                                            creep.moveTo(attackStructureResult, {reusePath: 3, ignoreRoads: true, ignoreDestructibleStructures: true});
+                                            creep.moveTo(targetSpawn, {reusePath: 3, ignoreRoads: true, ignoreDestructibleStructures: true});
                                             break;
                                         case 0: // returns OK
                                             //creep.say something here using prototype.creepSpeech.js
