2653af7fd7de6dec65f88a4f02fb59626ffc6ab0
Daniel Reid <danielselareid40@gmail.com>
2017-02-27 15:12:40 +0700

    don’t need to do this every tick

--- prototype.room.js
+++ prototype.room.js
@@ -84,7 +84,7 @@ module.exports = function () {
             //hostile creeps
             global[this.name].creepsNotMine = this.find(FIND_HOSTILE_CREEPS);
             //extensions
-            global[this.name].extensions = this.find(FIND_MY_STRUCTURES, {filter: (s) => s.structureType == STRUCTURE_EXTENSION});
+            if (!global[this.name].extensions) global[this.name].extensions = this.find(FIND_MY_STRUCTURES, {filter: (s) => s.structureType == STRUCTURE_EXTENSION});
             //links
             global[this.name].links = this.find(FIND_MY_STRUCTURES, {filter: (s) => s.structureType == STRUCTURE_LINK});
             //containers
