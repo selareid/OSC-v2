52394aae390ec2e6749be417df12bde90c3cec11
Daniel Reid <danielselareid40@gmail.com>
2017-03-07 16:38:11 +0700

    now I don’t need a storage to remote mine :)

--- role.remoteHauler.js
+++ role.remoteHauler.js
@@ -62,96 +62,111 @@ module.exports = {
         }
 
         if (creep.memory.goingHome === true) {
-            if (creep.pos.roomName != creep.memory.room) {
-                if (creep.carry.energy > 0) {
-                    var constructionSite = creep.pos.findClosestByRange(FIND_MY_CONSTRUCTION_SITES);
-                    if (constructionSite) {
-                        if (creep.build(constructionSite) == ERR_NOT_IN_RANGE) {
-                            creep.moveTo(constructionSite);
-                        }
-                    }
-                    else {
-                        var needingRepair = creep.pos.findClosestByRange([creep.pos.findInRange(FIND_STRUCTURES, 3, {filter: (s) => s.hits < s.hitsMax}),
-                            creep.room.find(FIND_STRUCTURES, {filter: (s) => s.hits < s.hitsMax * 0.1})]);
-                        if (needingRepair) {
-                            if (creep.repair(needingRepair) == ERR_NOT_IN_RANGE) {
-                                creep.moveTo(needingRepair);
+            (function () {
+                if (creep.pos.roomName != creep.memory.room) {
+                    if (creep.carry.energy > 0) {
+                        var constructionSite = creep.pos.findClosestByRange(FIND_MY_CONSTRUCTION_SITES);
+                        if (constructionSite) {
+                            if (creep.build(constructionSite) == ERR_NOT_IN_RANGE) {
+                                creep.moveTo(constructionSite);
                             }
                         }
                         else {
-                            creep.moveTo(Game.rooms[creep.memory.room].find(FIND_MY_SPAWNS)[0], {reusePath: 37});
+                            var needingRepair = creep.pos.findClosestByRange([creep.pos.findInRange(FIND_STRUCTURES, 3, {filter: (s) => s.hits < s.hitsMax}),
+                                creep.room.find(FIND_STRUCTURES, {filter: (s) => s.hits < s.hitsMax * 0.1})]);
+                            if (needingRepair) {
+                                if (creep.repair(needingRepair) == ERR_NOT_IN_RANGE) {
+                                    creep.moveTo(needingRepair);
+                                }
+                            }
+                            else {
+                                creep.moveTo(Game.rooms[creep.memory.room].find(FIND_MY_SPAWNS)[0], {reusePath: 37});
+                            }
                         }
                     }
+                    else creep.moveTo(Game.rooms[creep.memory.room].find(FIND_MY_SPAWNS)[0], {reusePath: 37});
                 }
-                else creep.moveTo(Game.rooms[creep.memory.room].find(FIND_MY_SPAWNS)[0], {reusePath: 37});
-            }
-            else {
-                var links = _.filter(global[room.name].links, (l) => l.energy < l.energyCapacity);
-                var storage = room.storage;
+                else {
+                    var links = _.filter(global[room.name].links, (l) => l.energy < l.energyCapacity);
+                    var storage = room.storage;
 
-                var arrayOfBoth = links;
-                if (storage && _.sum(storage.store) < storage.storeCapacity) arrayOfBoth.push(storage);
+                    var arrayOfBoth = links;
+                    if (storage && _.sum(storage.store) < storage.storeCapacity) arrayOfBoth.push(storage);
 
-                var closer = creep.pos.findClosestByPath(arrayOfBoth);
+                    var closer = creep.pos.findClosestByPath(arrayOfBoth);
 
-                if (closer) {
-                    for (let resourceType in creep.carry) {
-                        if (creep.transfer(closer, resourceType) == ERR_NOT_IN_RANGE) {
-                            creep.moveTo(closer, {reusePath: 19});
+                    if (closer) {
+                        for (let resourceType in creep.carry) {
+                            if (creep.transfer(closer, resourceType) == ERR_NOT_IN_RANGE) {
+                                creep.moveTo(closer, {reusePath: 19});
+                                break;
+                            }
                         }
                     }
-                }
-                else {
-                    for (let resourceType in creep.carry) {
-                        creep.drop(resourceType);
-                    }
-                }
-            }
-        }
-        else {
-            if (creep.pos.roomName != remoteFlag.pos.roomName) {
-                creep.moveTo(remoteFlag.pos, {reusePath: 37});
-            }
-            else {
-                let allDroppedResources = _.max(creep.room.find(FIND_DROPPED_RESOURCES), '.amount');
-                var droppedResource = allDroppedResources !== Number.POSITIVE_INFINITY && allDroppedResources !== Number.NEGATIVE_INFINITY ? allDroppedResources : undefined;
-                if (droppedResource) {
-                    var pickupResult = creep.pickup(droppedResource);
-                    switch (pickupResult) {
-                        case 0:
-                            delete creep.memory.container;
-                            break;
-                        case -9:
-                            creep.moveTo(droppedResource, {reusePath: 7});
-                            break;
-                    }
-
-                }
-                else {
-
-                    var container = creep.pos.findClosestByRange(FIND_STRUCTURES, {filter: (s) => s.structureType == STRUCTURE_CONTAINER && _.sum(s.store) > 0});
-
-                    if (container) {
-                        var containerStore = _.sum(container.store);
-                        if (containerStore > 0) {
-                            if (container) {
-                                for (let resourceType in container.store) {
-                                    if (containerStore <= 0) {
-                                        break;
-                                    }
-                                    else if (creep.withdraw(container, resourceType) == ERR_NOT_IN_RANGE) {
-                                        creep.moveTo(container, {reusePath: 10});
-                                    }
+                    else {
+                        var container = creep.pos.findClosestByRange(global[room.name].containers, {filter: (c) => c.energy < c.energyCapacitys});
+                        if (container) {
+                            for (let resourceType in creep.carry) {
+                                if (creep.transfer(container, resourceType) == ERR_NOT_IN_RANGE) {
+                                    creep.moveTo(container, {reusePath: 19});
+                                    break;
                                 }
                             }
                         }
                         else {
-                            delete creep.memory.container;
+                            for (let resourceType in creep.carry) {
+                                creep.drop(resourceType);
+                            }
                         }
                     }
                 }
-            }
+            })();
         }
+        else {
+            (function () {
+                if (creep.pos.roomName != remoteFlag.pos.roomName) {
+                    creep.moveTo(remoteFlag.pos, {reusePath: 37});
+                }
+                else {
+                    let allDroppedResources = _.max(creep.room.find(FIND_DROPPED_RESOURCES), '.amount');
+                    var droppedResource = allDroppedResources !== Number.POSITIVE_INFINITY && allDroppedResources !== Number.NEGATIVE_INFINITY ? allDroppedResources : undefined;
+                    if (droppedResource) {
+                        var pickupResult = creep.pickup(droppedResource);
+                        switch (pickupResult) {
+                            case 0:
+                                delete creep.memory.container;
+                                break;
+                            case -9:
+                                creep.moveTo(droppedResource, {reusePath: 7});
+                                break;
+                        }
 
+                    }
+                    else {
+
+                        var container = creep.pos.findClosestByRange(global[room.name].containers, {filter: (s) => _.sum(s.store) > 0});
+
+                        if (container) {
+                            var containerStore = _.sum(container.store);
+                            if (containerStore > 0) {
+                                if (container) {
+                                    for (let resourceType in container.store) {
+                                        if (containerStore <= 0) {
+                                            break;
+                                        }
+                                        else if (creep.withdraw(container, resourceType) == ERR_NOT_IN_RANGE) {
+                                            creep.moveTo(container, {reusePath: 10});
+                                        }
+                                    }
+                                }
+                            }
+                            else {
+                                delete creep.memory.container;
+                            }
+                        }
+                    }
+                }
+            })();
+        }
     }
 };
