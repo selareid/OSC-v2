ef9ed4158aa671d458fc7f921815a5e33563632b
Daniel Reid <danielselareid40@gmail.com>
2017-03-28 20:09:17 +0700

    extension filling :)

--- role.special.harvester.js
+++ role.special.harvester.js
@@ -1,11 +1,9 @@
 require('global');
 require('prototype.creepSpeech')();
 
-const roleDistributor = require('role.distributor');
-
 module.exports = {
     run: function (room, creep) {
-        
+
         if (creep.memory.w == true && _.sum(creep.carry) == 0) {
             creep.memory.w = false;
         }
@@ -17,20 +15,30 @@ module.exports = {
         var doing = creep.memory.d;
 
         switch (doing) {
+            case 'filler':
+                creep.say('filler');
+                //this.filler(room, creep);
+                break;
             case 'harvest':
                 creep.say('harvest');
                 this.harvest(room, creep);
                 break;
             default:
-                if (this.harvest(room, creep) == OK) {
-                    creep.memory.doing = 'harvest';
+                if (room.storage.store.energy < 400) {
+                    if (this.harvest(room, creep) == OK) {
+                        creep.memory.doing = 'harvest';
+                    }
                 }
                 else {
-                    roleDistributor.run(room, creep, 2090);
+                    if (this.filler(room, creep) == OK) {
+                        creep.memory.doing = 'filler';
+                    }
+                    else if (this.harvest(room, creep) == OK) {
+                        creep.memory.doing = 'harvest';
+                    }
                 }
         }
-        
-        
+
     },
     
     harvest: function (room, creep) {
@@ -52,8 +60,7 @@ module.exports = {
             switch (reslt) {
                 case ERR_NOT_IN_RANGE:
                     creep.creepSpeech(room, 'movingToSource');
-                    creep.moveTo(source, {reusePath: 10});
-                    reslt = OK;
+                    reslt = creep.moveTo(source, {reusePath: 10});
                     break;
                 case OK:
                     creep.creepSpeech(room, 'harvesting');
@@ -79,6 +86,55 @@ module.exports = {
             }
 
             return result;
+        }
+
+    },
+
+    getEnergy: function (room, creep) {
+        var storage = room.storage;
+
+        if (!storage) return 'error no storage';
+
+        var result = creep.withdraw(storage, RESOURCE_ENERGY);
+        switch (result) {
+            case ERR_NOT_IN_RANGE:
+                creep.creepSpeech(room, 'movingToEnergy');
+                result = creep.moveTo(storage, {reusePath: 10});
+                break;
+            case OK:
+                creep.creepSpeech(room, 'movingToEnergy');
+                break;
+        }
+
+        return result;
+    },
+
+    findSpawnExtension: function (room, creep) {
+        var spawns = _.filter(global[room.name].spawns, (s) => s.energy < s.energyCapacity);
+        var extensions = _.filter(global[room.name].extensions, (s) => s.energy < s.energyCapacity);
+        return creep.pos.findClosestByRange(spawns.concat(extensions));
+    },
+
+    filler: function (room, creep) {
+        if (room.energyAvailable >= room.energyCapacityAvailable) return 'nothing to do';
+
+        if (creep.memory.w == false) return this.getEnergy(room, creep);
+        else {
+            var spawnExtension = this.findSpawnExtension(room, creep);
+
+            if (spawnExtension) {
+                if (creep.transfer(spawnExtension, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(spawnExtension);
+                }
+            }
+            else {
+                var tower = _.filter(global[room.name].towers, (t) => t.energy < t.energyCapacity);
+                if (tower) {
+                    if (creep.transfer(tower, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(tower);
+                    }
+                }
+            }
         }
 
     }
