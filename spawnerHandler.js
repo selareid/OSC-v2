5a71af1d910e05c4fcec614c845bb98fd404b178
Daniel Reid <danielselareid40@gmail.com>
2017-04-20 18:58:07 +0700

    patched bug

--- spawnerHandler.js
+++ spawnerHandler.js
@@ -277,10 +277,10 @@ module.exports = {
                             break;
                     }
 
-                    temp_minimumNumberOfRemoteHarvesters += temp_harvesters;
-                    temp_minimumNumberOfRemoteMiners += temp_miners;
-                    temp_minimumNumberOfRemoteHaulers += temp_haulers;
-                    temp_minimumNumberOfRemoteGuards += temp_guards;
+                    temp_minimumNumberOfRemoteHarvesters += Number.parseInt(temp_harvesters);
+                    temp_minimumNumberOfRemoteMiners += Number.parseInt(temp_miners);
+                    temp_minimumNumberOfRemoteHaulers += Number.parseInt(temp_haulers);
+                    temp_minimumNumberOfRemoteGuards += Number.parseInt(temp_guards);
                 }
 
                 minimumNumberOfRemoteHarvesters = temp_minimumNumberOfRemoteHarvesters;
