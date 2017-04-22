77d43003fbac4a18c7fccfe7da98fecfdbbc74c2
Daniel Reid <danielselareid40@gmail.com>
2017-04-22 15:26:31 +0700

    ?

---
+++ role.remoteGuard.js
@@ -0,0 +1,151 @@
+require('global');
+require('prototype.creep');
+require('prototype.creepSpeech')();
+require('prototype.creepWar')();
+
+module.exports = {
+    run: function (room, creep) {
+        var remoteRooms = Memory.rooms[room].rmtR;
+
+        if (!remoteRooms || remoteRooms.length > 0) {
+            var remoteRoom = creep.memory.rr;
+
+            if (!remoteRoom) {
+                creep.memory.rr = this.setRemoteRoomMemory(room, creep, remoteRooms);
+                remoteRoom = creep.memory.rr;
+            }
+
+            if (remoteRoom) {
+                if (creep.pos.roomName != remoteRoom) {
+                    creep.moveTo(new RoomPosition(25, 25, remoteRoom), {reusePath: 21, range: 20, ignoreRoads: true});
+                }
+                else {
+                    this.realRun(room, creep);
+                }
+            }
+            else {
+                creep.runInSquares();
+            }
+
+        }
+        else {
+            creep.runInSquares();
+        }
+    },
+
+    setRemoteRoomMemory: function (room, creep, remoteRooms) {
+
+        var zeChosenRoom;
+
+        for (let rr of remoteRooms) {
+            let rrSpilt = rr.split(',');
+            let rroomName = rrSpilt[0];
+            let guardRoom = rrSpilt[3] < 1 || !rrSpilt[3];
+
+            if (guardRoom === true) continue;
+
+            let amountOfCreepsAssignedToThisRoom = _.filter(Game.creeps, (c) => c.memory.room == room.name && c.memory.role == 'remoteGuard' && c.memory.rr == rroomName).length;
+
+            if (amountOfCreepsAssignedToThisRoom < 1) {
+                zeChosenRoom = rroomName;
+                break;
+            }
+        }
+
+        return zeChosenRoom;
+    },
+
+    realRun: function (room, creep) {
+        creep.creepSpeech(room);
+
+        var target = this.getTarget(creep.room, creep);
+        if (target) {
+
+            var attackResult = creep.rangedAttack(target);
+
+            switch (attackResult) {
+                case -9: // returns ERR_NOT_IN_RANGE
+                    creep.moveTo(target, {reusePath: 3, ignoreRoads: true});
+                    break;
+                case 0: // returns OK
+                    creep.kite(creep.room, creep, target);
+                    //creep.say something here using prototype.creepSpeech.js
+                    break;
+                default:
+                    global.creepErrorLog('Attack Error: ' + attackResult, creep, room);
+            }
+
+        }
+        else {
+            if (creep.hasActiveBodyparts(HEAL) && creep.hits < creep.hitsMax) creep.heal(creep);
+
+            var SKL = room.find(FIND_STRUCTURES, {filter: (s) => s.structureType == STRUCTURE_KEEPER_LAIR});
+            if (SKL.length > 0) {
+                var next = _.min(SKL, 'ticksToSpawn');
+                if (!global.isUndefinedOrNull(next)) {
+                    creep.moveTo(next, {reusePath: 3, ignoreRoads: true})
+                }
+            }
+        }
+    },
+
+    getTarget: function (room, creep) {
+        var targets = _.filter(room.find(FIND_HOSTILE_CREEPS), (c) => !global.Allies.includes(c.owner.username));
+
+        if (targets.length > 0) {
+            return creep.pos.findClosestByRange(targets);
+        }
+    },
+
+    virtualMove: function (pos, dir) {
+        var tempPos;
+        var newPos;
+
+        if (!pos) return;
+
+        tempPos = pos;
+
+        switch (dir) {
+            case TOP:
+                tempPos.y = tempPos.y - 1;
+                break;
+            case TOP_RIGHT:
+                tempPos.y = tempPos.y - 1;
+                tempPos.x = tempPos.x + 1;
+                break;
+            case RIGHT:
+                tempPos.x = tempPos.x + 1;
+                break;
+            case BOTTOM_RIGHT:
+                tempPos.y = tempPos.y + 1;
+                tempPos.x = tempPos.x + 1;
+                break;
+            case BOTTOM:
+                tempPos.y = tempPos.y + 1;
+                break;
+            case BOTTOM_LEFT:
+                tempPos.y = tempPos.y + 1;
+                tempPos.x = tempPos.x - 1;
+                break;
+            case LEFT:
+                tempPos.x = tempPos.x - 1;
+                break;
+            case TOP_LEFT:
+                tempPos.y = tempPos.y - 1;
+                tempPos.x = tempPos.x - 1;
+                break;
+            default:
+                return;
+        }
+
+        newPos = tempPos;
+
+        return newPos;
+    }
+};
+
+
+/*
+ *Notes:
+ * global.Allies
+ */
