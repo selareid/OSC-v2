7448d10d0097035f69f2720ba0b3ce8f2867bc82
Daniel Reid <danielselareid40@gmail.com>
2017-04-24 15:48:51 +0700

    lets give this a shot

--- global.js
+++ global.js
@@ -312,3 +312,46 @@ global.roadTest = function (roomName) {
 
     });
 };
+
+global.remoteRoads = function (start, goal) {
+
+    var radius = 0.3;
+    var style;
+    goal = {pos: goal, range: 1};
+
+    let ret = PathFinder.search(
+        start, goal,
+        {
+            // We need to set the defaults costs higher so that we
+            // can set the road cost lower in `roomCallback`
+            plainCost: 2,
+            swampCost: 10,
+
+            roomCallback: function (roomName) {
+
+                let room = Game.rooms[roomName];
+                // In this example `room` will always exist, but since
+                // PathFinder supports searches which span multiple rooms
+                // you should be careful!
+                if (!room) return;
+                let costs = new PathFinder.CostMatrix;
+
+                room.find(FIND_STRUCTURES).forEach(function (struct) {
+                    if (struct.structureType === STRUCTURE_ROAD) {
+                        // Favor roads over plain tiles
+                        costs.set(struct.pos.x, struct.pos.y, 1);
+                    } else if (struct.structureType !== STRUCTURE_CONTAINER &&
+                        (struct.structureType !== STRUCTURE_RAMPART || !struct.my)) {
+                        // Can't walk through non-walkable buildings
+                        costs.set(struct.pos.x, struct.pos.y, 0xff);
+                    }
+                });
+
+                return costs;
+            },
+        });
+
+    _.forEach(ret.path, (pos) => {
+        pos.roomName.visual.circle(pos.x, pos.y, {radius: radius, lineStyle: style});
+    });
+};
