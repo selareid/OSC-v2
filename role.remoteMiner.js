1b206c358a517534855a7d3fd9e753359b0d34f6
Daniel Reid <danielselareid40@gmail.com>
2017-04-26 12:00:53 +0700

    run away from hostiles

--- role.remoteMiner.js
+++ role.remoteMiner.js
@@ -112,6 +112,53 @@ module.exports = {
                             break;
                     }
                 }
+                else {
+                    var goals = _.map(hostilesNearSource, function(hostile) {
+                        return { pos: hostile.pos, range: 6};
+                    });
+                    var ret = PathFinder.search(
+                        creep.pos, goals,
+                        {
+                            // We need to set the defaults costs higher so that we
+                            // can set the road cost lower in `roomCallback`
+                            plainCost: 2,
+                            swampCost: 10,
+                            flee: true,
+
+                            roomCallback: function(roomName) {
+
+                                let room = Game.rooms[roomName];
+                                // In this example `room` will always exist, but since
+                                // PathFinder supports searches which span multiple rooms
+                                // you should be careful!
+                                if (!room) return;
+                                let costs = new PathFinder.CostMatrix;
+
+                                room.find(FIND_STRUCTURES).forEach(function(struct) {
+                                    if (struct.structureType === STRUCTURE_ROAD) {
+                                        // Favor roads over plain tiles
+                                        costs.set(struct.pos.x, struct.pos.y, 1);
+                                    } else if (struct.structureType !== STRUCTURE_CONTAINER &&
+                                        (struct.structureType !== STRUCTURE_RAMPART ||
+                                        !struct.my)) {
+                                        // Can't walk through non-walkable buildings
+                                        costs.set(struct.pos.x, struct.pos.y, 0xff);
+                                    }
+                                });
+
+                                // Avoid creeps in the room
+                                room.find(FIND_CREEPS).forEach(function(creep) {
+                                    costs.set(creep.pos.x, creep.pos.y, 0xff);
+                                });
+
+                                return costs;
+                            },
+                        }
+                    );
+
+                    var pos = ret.path[0];
+                    creep.move(creep.pos.getDirectionTo(pos));
+                }
             }
             else remoteHarvester.realRun(room, creep);
         }
