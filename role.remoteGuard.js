cff257a6e77c773f30862187e927faa1565bf563
Daniel <danielselareid40@gmail.com>
2017-05-11 13:21:46 +0700

    Update role.remoteGuard.js

--- role.remoteGuard.js
+++ role.remoteGuard.js
@@ -65,7 +65,7 @@ module.exports = {
             if (creep.hasActiveBodyparts(HEAL) && creep.hits < creep.hitsMax*0.5) creep.heal(creep);
             else {
                 var rsl = creep.attack(target);
-                if (rsl != OK && creep.hasActiveBodyparts(HEAL) && creep.hits < creep.hitsMax) creep.heal(creep);
+                if (rsl != OK && creep.hasActiveBodyparts(HEAL)) creep.heal(creep);
             }
             creep.moveTo(target, {reusePath: 3, ignoreRoads: true});
         }
